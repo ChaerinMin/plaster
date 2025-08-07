61046f91019d857dbac9eec798e607f7ea49f467
Srinath Sridhar <srinath+brics@brown.edu>
2025-08-07 12:45:21 -0700

    Progressing plaster

--- src/run_plaster.py
+++ src/run_plaster.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run Plaster with specified source.")
     parser.add_argument("source", type=str, help="Path to the source directory")
     args = parser.parse_args()
-    source_instance = source.Source(args.source)
+    source_instance = source.Source(args.source, force_reserialize=True)
--- src/source.py
+++ src/source.py
@@ -7,10 +7,9 @@ class Sensor:
     """
     A class representing a sensor that captures data.
     """
-    def __init__(self, name, source_path):
-        self.name = name
+    def __init__(self, source_path):
         self.source_path = source_path
-        self.data = []
+        self.name = os.path.basename(os.path.normpath(source_path))
         self.init()
 
     def init(self):
@@ -29,10 +28,13 @@ class Day:
     """
     A class representing a day of data captured by a BRICS rig.
     """
-    def __init__(self, date, source_path):
+    def __init__(self, date, source_path, force_reserialize=False):
         self.source_path = source_path
         self.date = date
+        self.path = os.path.join(source_path, date)
+        self.plaster_path = os.path.join(self.path, 'plaster.json')
         self.sensors = []
+        self.force_reserialize = force_reserialize
         self.init()
 
     def init(self):
@@ -40,17 +42,16 @@ class Day:
         Initializes the day by listing all the sensors that were capturing data on that day.
         """
         sensor_pattern = re.compile(r'^[A-Za-z0-9_\-]+$')
-        sensor_dir = os.path.join(self.source_path, self.date)
-        if not os.path.exists(sensor_dir):
+        if not os.path.exists(self.path):
             self.sensors = []
+            print(f"Day directory {self.path} does not exist.")
             return
 
-        self.sensors = [entry for entry in os.listdir(sensor_dir)
-                        if os.path.isdir(os.path.join(sensor_dir, entry)) and sensor_pattern.match(entry)]
+        self.sensors = [entry for entry in os.listdir(self.path)
+                        if os.path.isdir(os.path.join(self.path, entry)) and sensor_pattern.match(entry)]
 
-        plaster_path = os.path.join(sensor_dir, 'plaster.json')
-        if os.path.exists(plaster_path):
-            with open(plaster_path, 'r') as json_file:
+        if os.path.exists(self.plaster_path):
+            with open(self.plaster_path, 'r') as json_file:
                 try:
                     data = json.load(json_file)
                     json_sensors = data.get('sensors', [])
@@ -61,6 +62,11 @@ class Day:
                 self.sensors = json_sensors
                 return
 
+        self.serialize(self.plaster_path)
+
+    def serialize(self, plaster_path):
+        """Serializes the day's data to a JSON format in the day's directory.
+        """
         json_obj = json.dumps({
             "date": self.date,
             "sensors": self.sensors,
@@ -69,19 +75,17 @@ class Day:
         with open(plaster_path, 'w') as json_file:
             json_file.write(json_obj)
 
-    def get_date(self):
-        return self.date
-
 class Source:
     """
     A class representing the source of data, usually captured by a single BRICS rig (e.g., BRICS Mini, BRICS Studio).
     """
-    def __init__(self, path):
+    def __init__(self, path, force_reserialize=False):
         self.name = os.path.basename(os.path.normpath(path))
         self.path = path
         print(f"Initializing Source: {self.name} at {self.path}")
         self.days = []
         self.plaster_path = os.path.join(self.path, 'plaster.json')
+        self.force_reserialize = force_reserialize
         self.init()
 
     def init(self):
@@ -92,7 +96,7 @@ class Source:
         dir_days = [entry for entry in os.listdir(self.path)
                     if os.path.isdir(os.path.join(self.path, entry)) and date_pattern.match(entry)]
 
-        if os.path.exists(self.plaster_path):
+        if os.path.exists(self.plaster_path) and not self.force_reserialize:
             with open(self.plaster_path, 'r') as json_file:
                 try:
                     data = json.load(json_file)
@@ -101,23 +105,23 @@ class Source:
                     json_days = []
 
             if set(json_days) == set(dir_days):
-                self.days = [Day(date, self.path) for date in json_days]
+                self.days = [Day(date, self.path, self.force_reserialize) for date in json_days]
                 return
             # else, update JSON below
 
-        self.days = [Day(date, self.path) for date in dir_days]
-        self.serialize()
+        self.days = [Day(date, self.path, self.force_reserialize) for date in dir_days]
+        self.serialize(self.plaster_path)
 
-    def serialize(self):
+    def serialize(self, plaster_path):
         """
         Serializes the source data to a JSON format in the top-level directory.
         """
-        json_obj    = json.dumps({
+        json_obj = json.dumps({
             "name": self.name,
             "days": [day.get_date() for day in self.days],
             "plaster_timestamp": datetime.now().isoformat()
         }, indent=4)
-        with open(self.plaster_path, 'w') as json_file:
+        with open(plaster_path, 'w') as json_file:
             json_file.write(json_obj)
-        print(f"Serialized Source to {self.plaster_path}")
+        print(f"Serialized Source to {plaster_path}")
     
