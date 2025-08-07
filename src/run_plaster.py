d309f3b6ad8214b36fa223e649fc73560008f3a5
Srinath Sridhar <srinath+brics@brown.edu>
2025-08-07 12:51:06 -0700

    Progressing plaster

--- src/run_plaster.py
+++ src/run_plaster.py
@@ -1,8 +1,10 @@
 import source
 import argparse
 
+parser = argparse.ArgumentParser(description="Run Plaster with specified source.")
+parser.add_argument("-s", "--source", type=str, help="Path to the source directory")
+parser.add_argument("-f", "--force-reserialize", action="store_true", help="Force reserialization of the source")
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Run Plaster with specified source.")
-    parser.add_argument("source", type=str, help="Path to the source directory")
     args = parser.parse_args()
-    source_instance = source.Source(args.source, force_reserialize=True)
+    source_instance = source.Source(args.source, force_reserialize=args.force_reserialize)
