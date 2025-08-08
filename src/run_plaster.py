7b63a88ee095a2ed59986c9df89028e8cc3f0db5
Srinath Sridhar <srinath+brics@brown.edu>
2025-08-08 15:38:03 -0700

    Adding microsecond support

--- src/run_plaster.py
+++ src/run_plaster.py
@@ -7,11 +7,13 @@ parser.add_argument("-f", "--force-reserialize", action="store_true", help="Forc
 # Take an argument for time stamp units
 parser.add_argument("-t", "--time-stamp-units", type=str, choices=["microseconds", "nanoseconds"], default="nanoseconds", help="Time stamp units")
 
+TIME_THRESHOLD_S = 5 # THRESHOLD in seconds
+TIME_MULTIPLIER = 1e9 # Nanoseconds by default
+
 if __name__ == "__main__":
     args = parser.parse_args()
 
     global TIME_MULTIPLIER, TIME_THRESHOLD_S
-    TIME_THRESHOLD_S = 5 # THRESHOLD in seconds
     if args.time_stamp_units == "microseconds":
         print("Using microseconds for time stamps.")
         TIME_MULTIPLIER = 1e6
