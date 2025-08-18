ac3ba91cfecc1d103e90fea3b900638d944463ed
Srinath Sridhar <srinath+brics@brown.edu>
2025-08-17 21:15:41 -0700

    testing on brics studio

--- plaster_space.py
+++ plaster_space.py
@@ -33,13 +33,13 @@ if __name__ == "__main__":
     if True:
         if True:
             ms = dict()
-            # args.source = "~/data/brics/non-pii/brics-studio"
-            # day = "2025-03-28" # brics-studio, multisequence000001
-            # ms["name"] = "multisequence000001"
+            args.source = "~/data/brics/non-pii/brics-studio"
+            day = "2025-03-28" # brics-studio, multisequence000001
+            ms["name"] = "multisequence000001"
             # Lawn
-            args.source = "/oscar/data/ssrinath/brics/non-pii/brics-universe"
-            day = "2025-05-14" # brics-universe, multisequence000003
-            ms["name"] = "multisequence000003"
+            # args.source = "/oscar/data/ssrinath/brics/non-pii/brics-universe"
+            # day = "2025-05-14" # brics-universe, multisequence000003
+            # ms["name"] = "multisequence000003"
             # # 191 Medway
             # args.source = "/oscar/data/ssrinath/brics/non-pii/brics-universe"
             # day = "2025-05-11" # brics-universe, multisequence000003
