50ca454ba98e461851cb67d5c512c28f92a6da13
Srinath Sridhar <srinath+brics@brown.edu>
2025-09-19 19:31:09 -0400

    changing feature extractor

--- vggt_colmap.py
+++ vggt_colmap.py
@@ -224,7 +224,7 @@ def run_vggt_calibration(args):
                     masks=None,
                     max_query_pts=args.max_query_pts,
                     query_frame_num=args.query_frame_num,
-                    keypoint_extractor="aliked+sp",
+                    keypoint_extractor="aliked+sp+sift",
                     fine_tracking=args.fine_tracking,
                 )
                 
